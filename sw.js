490b6f0495ed3a6543b00db7a6d4c2b7b0403773
Oksana-Pl <k00299681@student.tus.ie>
2026-07-17 10:01:18 +0100

    manifest

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "climate-finance-v1";
+
+const APP_ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.webmanifest",
+    "./css/slyle.css",
+    "./js/main.js?v=2",
+    "./images/app-icon-192.png",
+    "./images/app-icon-512.png",
+    "./images/background.png",
+    "./images/coin.svg",
+    "./images/coins.png",
+    "./images/coins.svg",
+    "./images/countries.svg",
+    "./images/settings.svg"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames
+                .filter((cacheName) => cacheName !== CACHE_NAME)
+                .map((cacheName) => caches.delete(cacheName))
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            fetch(event.request).catch(() => caches.match("./index.html"))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request).then((networkResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
+});
